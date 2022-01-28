96240df86e66113bb27e1c4f92d8b6fe32592c16
Shanavazh <shanavazh@gmail.com>
2022-01-28 14:18:57 +0530

    adding build script

---
+++ build_layer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# set -o errexit
+# set -o nounset
+mkdir -p build/python
+pip3 install -t ./build/python prometheus-client
+cp -R asserts_pylambda ./build/python
+cd build
+zip -r ../asserts-sdk-python-1.0.0.zip ./python
+cd ..
+rm -fR build
